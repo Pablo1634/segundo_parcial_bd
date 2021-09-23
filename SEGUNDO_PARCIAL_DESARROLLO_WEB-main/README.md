b291b84afbc9a519acbc4199ca509fb632a6c91b
Pablo1634 <80859246+Pablo1634@users.noreply.github.com>
2021-09-22 18:41:52 -0600

    Add files via upload

---
+++ SEGUNDO_PARCIAL_DESARROLLO_WEB-main/README.md
@@ -0,0 +1,3 @@
+# SEGUNDO_PARCIAL_DESARROLLO_WEB
+CRUD de Productos en PHP
+Pablo Raul Arroyave Echeverria		1290-16-3035
---
+++ SEGUNDO_PARCIAL_DESARROLLO_WEB-main/datos_conexion.php
@@ -0,0 +1,8 @@
+<!-- Datos de Conexion a Base de Datos MySql-->
+<?php 
+    $db_host = "localhost";
+    $db_usr = "root";
+    $db_pass = "Pablo163*";
+    $db_name = "segundoparcial";
+    $db_port = "8080";
+?>
